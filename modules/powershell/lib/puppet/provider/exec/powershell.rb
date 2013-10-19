c8da42c2862a5aea75cb0a70ee13396ea0643a21
pigram86 <todd@toddpigram.com>
2013-10-19 18:40:18 -0400

    initial commit

---
+++ modules/powershell/lib/puppet/provider/exec/powershell.rb
@@ -0,0 +1,40 @@
+require 'puppet/provider/exec'
+
+Puppet::Type.type(:exec).provide :powershell, :parent => Puppet::Provider::Exec do
+  confine :operatingsystem => :windows
+
+  POWERSHELL =
+    if File.exists?("#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe")
+      "#{ENV['SYSTEMROOT']}\\sysnative\\WindowsPowershell\\v1.0\\powershell.exe"
+    elsif File.exists?("#{ENV['SYSTEMROOT']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe")
+      "#{ENV['SYSTEMROOT']}\\system32\\WindowsPowershell\\v1.0\\powershell.exe"
+    else
+      "powershell.exe"
+    end
+
+  commands :powershell => POWERSHELL
+
+  desc <<-EOT
+    Executes Powershell commands. One of the `onlyif`, `unless`, or `creates`
+    parameters should be specified to ensure the command is idempotent.
+
+    Example:
+        # Rename the Guest account
+        exec { 'rename-guest':
+          command   => '$(Get-WMIObject Win32_UserAccount -Filter "Name=\'guest\'").Rename("new-guest")',
+          unless    => 'if (Get-WmiObject Win32_UserAccount -Filter "Name=\'guest\'") { exit 1 }'
+          provider  => powershell
+        }
+  EOT
+
+  def run(command, check = false)
+    super("\"#{POWERSHELL}\" #{command}", check)
+  end
+
+  def checkexe(command)
+  end
+
+  def validatecmd(command)
+    true
+  end
+end
---
+++ modules/win_facts/README.md
@@ -0,0 +1,9 @@
+Puppet module with additional useful windows facts
+
+This module is also available on the [Puppet Forge](https://forge.puppetlabs.com/liamjbennett/win_facts)
+
+## Facts ##
+* operatingsystemversion - 
+* windows_productkey -
+* windows_sid -
+* windows_systemtype -
